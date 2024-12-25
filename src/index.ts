3565373840285d84cd8a934d539c5060c759ae22
Ankit Sharma <ankitsharma290101@gmail.com>
2024-12-25 20:06:03 +0530

    routes

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,10 @@
 import express from "express";
-
+import mongoose from "mongoose";
 const app = express();
 
-app.get();
+app.post("/api/v1/signup", (req, res) => {});
+app.post("/api/v1/signin", (req, res) => {});
+app.get("/api/v1/content", (req, res) => {});
+app.delete("/api/v1/content", (req, res) => {});
+app.post("/api/v1/brain/share", (req, res) => {});
+app.get("/api/v1/brain/:shareLink", (req, res) => {});
