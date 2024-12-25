977091fd940b8014fb6137df5bd720ba5ff73c05
Ankit Sharma <ankitsharma290101@gmail.com>
2024-12-25 21:46:15 +0530

    routes

--- src/db.ts
+++ src/db.ts
@@ -1,5 +1,7 @@
 import mongoose, { model, Schema, Types } from "mongoose";
 
+mongoose.connect("mongodb://localhost:27017/brainly_db");
+
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
