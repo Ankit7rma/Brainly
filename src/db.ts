16c2543a3fade6582f0909ed7127ad0f91aa820f
Ankit Sharma <ankitsharma290101@gmail.com>
2024-12-25 20:06:16 +0530

    schemas

---
+++ src/db.ts
@@ -0,0 +1,33 @@
+import mongoose, { Schema, Types } from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+});
+
+const contentTypes = ["image", "video", "article", "audio"];
+
+const contentSchema = new Schema({
+  link: { type: String, required: true },
+  type: { type: String, enum: contentTypes, required: true },
+  title: { type: String, required: true },
+  tags: [{ type: Types.ObjectId, ref: "Tag" }],
+  userId: { type: Types.ObjectId, ref: "User", required: true },
+});
+
+const LinkSchema = new Schema({
+  // 'hash' is a string that represents the shortened or hashed version of a link
+  hash: String,
+
+  // 'userId' is a reference to the 'User' collection in the database.
+  // It uses Mongoose's ObjectId type for relational data.
+  // The 'ref' property specifies the referenced collection name ('User').
+  // The 'required' property ensures this field must be provided when creating a document.
+  // The 'unique' property enforces that each 'userId' in this collection is unique.
+  userId: {
+    type: mongoose.Types.ObjectId,
+    ref: "User",
+    required: true,
+    unique: true,
+  },
+});
